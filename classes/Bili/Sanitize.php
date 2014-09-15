82540088c9bfa7e4f1ae136d834eb7535983a120
Felix Langfeldt <felix@neverwoods.com>
2014-09-15 11:05:44 -0400

    Fixed a bug with duplicate methods

--- classes/Bili/Sanitize.php
+++ classes/Bili/Sanitize.php
@@ -151,13 +151,6 @@ class Sanitize
         return $varReturn;
     }
 
-    public static function toFilename($strOutput)
-    {
-    	$strOutput = preg_replace('/([^\w\s\d\-\.%_~,;:\[\]\|])/u', '', $strOutput);
-
-    	return $strOutput;
-    }
-
     /**
      * Sanitize a string to a pure ascii string. No special characters or any other fancy UTF-8 stuff.
      *
