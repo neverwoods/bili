a25e44ae588f8208d21306b82926ed80664c90b7
Felix Langfeldt <felix@neverwoods.com>
2017-09-25 21:06:52 +0200

    Adds documentation to the BubbleMessage class

--- classes/Bili/BubbleMessage.php
+++ classes/Bili/BubbleMessage.php
@@ -6,6 +6,26 @@ namespace Bili;
  * Class to hold the message logic.
  *
  * @package Bili
+ *
+ * @method void setMessage(string $value) Sets the message body
+ * @method string getMessage() Returns the message body
+ * @method void setTitle(string $value) Sets the message title
+ * @method string getTitle() Returns the message title
+ * @method void setType(string $value) Sets the message type
+ * @method string getType() Returns the message type
+ * @method void setIcon(string $value) Sets the message icon
+ * @method string getIcon() Returns the message icon
+ * @method void setLocation(int $value) Sets the message location
+ * @method int getLocation() Returns the message location
+ * @method void setTimeout(int $value) Sets the message time out in milliseconds
+ * @method int getTimeout() Returns the message time out in milliseconds
+ * @method void setPermanent(bool $value) Sets if the message should be shown permanently
+ * @method bool getPermanent() Returns if the message should be shown permanently
+ * @method void setDismiss(bool $value) Sets if the message can be dismissed
+ * @method bool getDismiss() Returns if the message can be dismissed
+ * @method void setKey(string $value) Sets the message key
+ * @method string getKey() Returns the message key
+ * @method string getId() Returns the message id
  */
 
 class BubbleMessage extends ClassDynamic implements \JsonSerializable
